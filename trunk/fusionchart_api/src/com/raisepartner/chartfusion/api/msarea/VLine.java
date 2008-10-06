bf8d8ee8f7a2c2986ae31ceacbc4103025506456
zlegein <zlegein@3e7d0e48-ab55-0410-a05d-cfcde8f17f65>
2008-10-06 19:26:04 +0000

    1 - add missing ojbect classes representing the xml.

---
+++ trunk/fusionchart_api/src/com/raisepartner/chartfusion/api/msarea/VLine.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2008 Raise Partner
+ * 22, av. Doyen Louis Weil,
+ * 38000 Grenoble, France
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Contact: sebastien@chassande.fr
+ */
+package com.raisepartner.chartfusion.api.msarea;
+
+public class VLine extends com.raisepartner.chartfusion.api.VLine {
+
+
+	public VLine() {
+	}
+}
---
+++ trunk/fusionchart_api/src/com/raisepartner/chartfusion/api/scatter/Category.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2008 Raise Partner
+ * 22, av. Doyen Louis Weil,
+ * 38000 Grenoble, France
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Contact: sebastien@chassande.fr
+ */
+package com.raisepartner.chartfusion.api.scatter;
+
+public class Category extends com.raisepartner.chartfusion.api.Category {
+
+	public final static String PARAMETER_X = "x";
+	public final static String PARAMETER_SHOWVERTICALLINE = "showVerticalLine";
+	public final static String PARAMETER_LINEDASHED = "lineDashed";
+
+	public Category() {
+	}
+
+	/**
+	 * Type:   Number   
+	 * Range value:   Numeric Value   
+	 * Description :
+	 *  The bubble/scatter chart have both x and y axis as numeric. This 
+	 * attribute lets you define the x value (numerical position on the x-
+	 * axis) where this category name would be placed.
+	 */
+	public void setX(String value) {
+		setAttribute(PARAMETER_X, value);
+	}
+
+	/**
+	 * Type: Boolean 
+	 * Range value: 0/1 
+	 * Description :
+	 * Whether the vertical line should be shown for this category?
+	 */
+	public void setShowVerticalLine(String value) {
+		setAttribute(PARAMETER_SHOWVERTICALLINE, value);
+	}
+
+	/**
+	 * Type: Boolean 
+	 * Range value: 0/1 
+	 * Description :
+	 * Whether the vertical line should appear as dashed?
+	 */
+	public void setLineDashed(String value) {
+		setAttribute(PARAMETER_LINEDASHED, value);
+	}
+}
