e48d73b2f171e97cfb58e0e853a80e5eb9505e18
bricesaccucci <bricesaccucci@3e7d0e48-ab55-0410-a05d-cfcde8f17f65>
2009-07-23 10:32:13 +0000

    Manage XML forbidden characters.

--- trunk/fusionchart_api/src/com/raisepartner/chartfusion/api/Node.java
+++ trunk/fusionchart_api/src/com/raisepartner/chartfusion/api/Node.java
@@ -34,40 +34,49 @@ import java.util.Map.Entry;
  */
 public class Node {
 
-    /**
-     * The node name
-     */
+	/**
+	 * The node name
+	 */
 	public String name;
-	
-    /**
-     * The list of subnodes
-     */
+
+	/**
+	 * The list of subnodes
+	 */
 	public final List<Node> subnodes = new ArrayList<Node>();
 
-    /**
-     * The attributes of the node.
-     */
+	/**
+	 * The attributes of the node.
+	 */
 	public final Properties attributes = new Properties();
-	
-    /**
-     * Builds a node
-     * @param name is the name of the node
-     */
+
+	/**
+	 * Builds a node
+	 * 
+	 * @param name
+	 *            is the name of the node
+	 */
 	public Node(String name) {
 		this.name = name;
 	}
-    /**
-     * Add a subnode.
-     * @param n is the new subnode
-     */
+
+	/**
+	 * Add a subnode.
+	 * 
+	 * @param n
+	 *            is the new subnode
+	 */
 	public void addNode(Node n) {
 		subnodes.add(n);
 	}
-    /**
-     * Defines an attribute. If the value is null, the attribute is removed.
-     * @param name is the attribute name
-     * @param value is the attribute value
-     */
+
+	/**
+	 * Defines an attribute. If the value is null, the attribute is removed.
+	 * 
+	 * @param name
+	 *            is the attribute name
+	 * @param value
+	 *            is the attribute value
+	 */
 	public void setAttribute(String name, String value) {
 		if (value == null) {
 			attributes.remove(name);
@@ -76,45 +85,51 @@ public class Node {
 		}
 	}
 
-    /**
-     * Build the xml using the default representation of the node.
-     * @see #toString
-     */
-    public String getDataUrl() {
-        return this.toString();
-    }
+	/**
+	 * Build the xml using the default representation of the node.
+	 * 
+	 * @see #toString
+	 */
+	public String getDataUrl() {
+		return this.toString();
+	}
 
-    /**
-     * Replaces all <i>&amp;</i> with <i>%26</i>, in order to be
-     * compatable with the <i>setDataXml</i> function in the FusionCharts.js.
-     */
-    public String getDataXMl() {
-        String xml = this.toString();
-        return xml.replaceAll("&", "%26");
-    }
+	/**
+	 * Replaces all <i>&amp;</i> with <i>%26</i>, in order to be compatable with
+	 * the <i>setDataXml</i> function in the FusionCharts.js.
+	 */
+	public String getDataXMl() {
+		String xml = this.toString();
+		return xml.replaceAll("&", "%26");
+	}
 
-    /**
-     * Builds the XML representation of the node.
-     */
+	/**
+	 * Builds the XML representation of the node.
+	 */
 	@Override
 	public String toString() {
-		return toString(new StringBuffer(), "").toString(); 
+		return toString(new StringBuffer(), "").toString();
 	}
-    /**
-     * Recursive method building XML representation of the node
-     * @param sb is the StringBuffer to fill with the representation of the 
-     * current node. Must not be null.
-     * @param tab is the tabulation prefix to insert at each line begin. Must 
-     * not be null. 
-     * @return the tab parameter.
-     */
+
+	/**
+	 * Recursive method building XML representation of the node
+	 * 
+	 * @param sb
+	 *            is the StringBuffer to fill with the representation of the
+	 *            current node. Must not be null.
+	 * @param tab
+	 *            is the tabulation prefix to insert at each line begin. Must
+	 *            not be null.
+	 * @return the tab parameter.
+	 */
 	private StringBuffer toString(StringBuffer sb, String tab) {
 		final String innerTab = tab + "\t";
 		sb.append(tab).append("<").append(name);
 		for (Entry<Object, Object> me : attributes.entrySet()) {
 			if (me.getValue() != null) {
 				sb.append(" ").append((String) me.getKey());
-				sb.append("='").append(encodeAttribute((String) me.getValue())).append("'");
+				sb.append("='").append(encodeAttribute((String) me.getValue()))
+						.append("'");
 			}
 		}
 		if (attributes.size() == 0) {
@@ -132,13 +147,39 @@ public class Node {
 		}
 		return sb;
 	}
-	
+
 	/**
-	 * Encode an attribute's value so that the XML is well-formed (avoid quotes for instance)
+	 * Encode an attribute's value so that the XML is well-formed (avoid quotes
+	 * for instance)
+	 * 
 	 * @param attributeValue
 	 * @return
 	 */
 	private String encodeAttribute(String attributeValue) {
-		return attributeValue.replaceAll("'", "&apos;");
+		StringBuffer resSB = new StringBuffer();
+		for (int i = 0; i < attributeValue.length(); i++) {
+			char c = attributeValue.charAt(i);
+			switch (c) {
+			case '&':
+				resSB.append("&amp;");
+				break;
+			case '<':
+				resSB.append("&lt;");
+				break;
+			case '>':
+				resSB.append("&gt;");
+				break;
+			case '"':
+				resSB.append("&quot;");
+				break;
+			case '\'':
+				resSB.append("&apos;");
+				break;
+			default:
+				resSB.append(c);
+				break;
+			}
+		}
+		return resSB.toString();
 	}
 }
